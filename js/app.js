56c7c48b713fccebfdea96056ad3f6c57d398b9d
César García <cesar.garciamcs@outlook.com>
2025-03-11 18:21:17 -0600

    llamando a la api key

--- js/app.js
+++ js/app.js
@@ -9,5 +9,46 @@ window.addEventListener('load', () => {
 function buscarClima(e) {
     e.preventDefault()
 
-    console.log('Buscando el clima')
+    // Validar
+    const ciudad = document.querySelector('#ciudad').value
+    const pais = document.querySelector('#pais').value
+
+    if(ciudad === '' || pais === '') {
+        mostrarAlerta('Todos los campos son obligatorios')
+        return
+    }
+
+    // Consultar API
+    consultarAPI(ciudad, pais)
+}
+
+function consultarAPI(ciudad, pais) {
+    const appId = config.API_KEY
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(datos => {
+            console.log(datos)
+            if(datos.cod === "404") {
+                mostrarAlerta('Ciudad no encontrada')
+                return
+            }
+            mostrarClima(datos)
+        })
+}
+
+function mostrarAlerta(mensaje) {
+    const alerta = document.querySelector('.alerta')
+    
+    if(!alerta) {
+        const alerta = document.createElement('p')
+        alerta.classList.add('bg-red-500', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-5', 'alerta')
+        alerta.textContent = mensaje
+        formulario.appendChild(alerta)
+
+        setTimeout(() => {
+            alerta.remove()
+        }, 3000)
+    }
 }
---
+++ js/config.js
@@ -0,0 +1,3 @@
+const config = {
+    API_KEY: '35892614ad788960429b3b264502ce47'
+};
