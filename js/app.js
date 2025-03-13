ea3864655bb1d3af6d3ab91e76121768823ed280
César García <cesar.garciamcs@outlook.com>
2025-03-12 21:10:38 -0600

    cambiando diseño en la busqueda y resultado

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.container')
 const resultado = document.querySelector('#resultado')
 const formulario = document.querySelector('#formulario')
+const buscador = document.querySelector('#buscador')
 
 window.addEventListener('load', () => {
     formulario.addEventListener('submit', buscarClima)
@@ -26,6 +27,9 @@ function consultarAPI(ciudad, pais) {
     const appId = config.API_KEY
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
 
+    // Muestra un spinner de carga
+    spinner()
+
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(datos => {
@@ -44,32 +48,42 @@ function consultarAPI(ciudad, pais) {
 function mostrarClima(datos) {
     const { name, main: { temp, temp_max, temp_min } } = datos
 
+    // Temperatura actual
     const actual = document.createElement('P')
-    actual.innerHTML = `${kelvinACentigrados(temp)} &#8451;`
-    actual.classList.add('font-bold', 'text-6xl', 'mb-4')
+    actual.innerHTML = `${kelvinACentigrados(temp)}°`
+    actual.classList.add('font-semibold', 'text-9xl')
 
+    // Titulo
     const nombreCiudad = document.createElement('P')
     nombreCiudad.textContent = `${name}`
-    nombreCiudad.classList.add('text-3xl', 'mb-4')
+    nombreCiudad.classList.add('font-semibold', 'text-6xl', 'mb-1')
 
+    // Temperatura actual
     const tempMaxima = document.createElement('P')
     tempMaxima.innerHTML = `Max: ${kelvinACentigrados(temp_max)} &#8451;`
     tempMaxima.classList.add('text-xl')
 
+    // Temperatura actual
     const tempMinima = document.createElement('P')
     tempMinima.innerHTML = `Min: ${kelvinACentigrados(temp_min)} &#8451;`
     tempMinima.classList.add('text-xl')
 
-    const resultadoMinMax = document.createElement('DIV')
-    resultadoMinMax.classList.add('flex', 'gap-5', 'justify-center')
-    resultadoMinMax.appendChild(tempMaxima)
-    resultadoMinMax.appendChild(tempMinima)
+    // Div de temperatura maxima y minima
+    const resultadoMaxMin = document.createElement('DIV')
+    resultadoMaxMin.classList.add('flex', 'gap-5', 'text-sm')
+    resultadoMaxMin.appendChild(tempMaxima)
+    resultadoMaxMin.appendChild(tempMinima)
 
+    // Div con la informacion
+    const infoDiv = document.createElement('DIV')
+    infoDiv.appendChild(nombreCiudad)
+    infoDiv.appendChild(resultadoMaxMin)
+
+    // Div contenedor
     const resultadoDiv = document.createElement('DIV')
-    resultadoDiv.classList.add('text-center', 'text-white')
-    resultadoDiv.appendChild(nombreCiudad)
+    resultadoDiv.classList.add('text-white', 'flex', 'gap-5', 'items-end')
     resultadoDiv.appendChild(actual)
-    resultadoDiv.appendChild(resultadoMinMax)
+    resultadoDiv.appendChild(infoDiv)
 
     resultado.appendChild(resultadoDiv)
 }
@@ -83,7 +97,7 @@ function mostrarAlerta(mensaje) {
         const alerta = document.createElement('p')
         alerta.classList.add('bg-red-500', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-5', 'alerta')
         alerta.textContent = mensaje
-        formulario.appendChild(alerta)
+        buscador.appendChild(alerta)
 
         setTimeout(() => {
             alerta.remove()
@@ -94,4 +108,18 @@ function limpiarHTML() {
     while(resultado.firstChild) {
         resultado.removeChild(resultado.firstChild)
     }
+}
+
+function spinner() {
+    limpiarHTML()
+
+    const spinnerDiv = document.createElement('DIV')
+    spinnerDiv.classList.add('spinner')
+    spinnerDiv.innerHTML = `
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+    `
+
+    resultado.appendChild(spinnerDiv)
 }
